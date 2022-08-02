c4c845cf93613034f5e6ef79c35c1b19ffb750f9
Jackson <jacksonxing7@gmail.com>
2022-08-02 15:52:36 +0700

    Added flights.py

---
+++ flights.py
@@ -0,0 +1,20 @@
+class Flight():
+    def __init__(self,capacity):
+        self.capacity = capacity
+        self.passengers = []
+    
+    def add_passengers(self,name):
+        if not self.open_seats():
+            return False
+        self.passengers.append(name)
+        return True
+    
+    def open_seats(self):
+        return self.capacity - len(self.passengers)
+flight = Flight(3)
+person = ["Jackson","Ron","Hermoine","Harry"]
+for people in person:
+    if flight.add_passengers(people):
+        print(f"{people} was succesfully add in the flight")
+    else:
+        print(f"Failed to added {people}")
